d18b7d920aadbb29d930cd6c1d441116316b6430
Katyara22 <irina.pryduvys@gmail.com>
2023-11-07 04:13:29 -0800

    add

---
+++ ServerManagement/src/main/java/katyara1/servermanagement/ServerManagement.java
@@ -0,0 +1,17 @@
+package katyara1.servermanagement;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ServerManagement extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
